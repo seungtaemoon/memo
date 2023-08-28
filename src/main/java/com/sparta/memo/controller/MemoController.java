b6e782d874c42547d41d467df1fc78cf6134b524
seungtaemoon <Robbie@email.com>
2023-08-28 23:50:53 +0900

    객체 중복 생성 문제 해결

--- src/main/java/com/sparta/memo/controller/MemoController.java
+++ src/main/java/com/sparta/memo/controller/MemoController.java
@@ -1,45 +1,31 @@
 package com.sparta.memo.controller;
-
 import com.sparta.memo.dto.MemoRequestDto;
 import com.sparta.memo.dto.MemoResponseDto;
-import com.sparta.memo.entity.Memo;
+import com.sparta.memo.service.MemoService;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.Collections;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
-
 @RestController
 @RequestMapping("/api")
 public class MemoController {
-
-    private final Map<Long, Memo> memoList = new HashMap<>();
-
+    private final MemoService memoService;
+    public MemoController(JdbcTemplate jdbcTemplate) {
+        this.memoService = new MemoService(jdbcTemplate);
+    }
     @PostMapping("/memos")
     public MemoResponseDto createMemo(@RequestBody MemoRequestDto requestDto) {
-        // RequestDto -> Entity
-        Memo memo = new Memo(requestDto);
-
-        // Memo Max ID Check (memoList에 데이터가 있는지 체크)
-        Long maxId = memoList.size() > 0 ? Collections.max(memoList.keySet()) + 1 : 1;
-        memo.setId(maxId);
-
-        // DB 저장
-        memoList.put(memo.getId(), memo);
-
-        // Entity -> ResponseDto
-        MemoResponseDto memoResponseDto = new MemoResponseDto(memo);
-
-        return memoResponseDto;
+        return memoService.createMemo(requestDto);
     }
-
     @GetMapping("/memos")
     public List<MemoResponseDto> getMemos() {
-        // Map To List (.stream을 활용해서 데이터를 순회하고 .map으로 생성자를 수행하여 dto로 만들고 list화)
-        List<MemoResponseDto> responseList = memoList.values().stream()
-                .map(MemoResponseDto::new).toList();
-
-        return responseList;
+        return memoService.getMemos();
+    }
+    @PutMapping("/memos/{id}")
+    public Long updateMemo(@PathVariable Long id, @RequestBody MemoRequestDto requestDto) {
+        return memoService.updateMemo(id, requestDto);
+    }
+    @DeleteMapping("/memos/{id}")
+    public Long deleteMemo(@PathVariable Long id) {
+        return memoService.deleteMemo(id);
     }
 }
--- src/main/java/com/sparta/memo/dto/MemoResponseDto.java
+++ src/main/java/com/sparta/memo/dto/MemoResponseDto.java
@@ -9,9 +9,15 @@ public class MemoResponseDto {
     private String username;
     private String contents;
 
-    public MemoResponseDto(Memo memo) {
-        this.id = memo.getId();
-        this.username = memo.getUsername();
-        this.contents = memo.getContents();
+    public MemoResponseDto(Long id, String username, String contents) {
+        this.id = id;
+        this.username = username;
+        this.contents = contents;
     }
+
+    public MemoResponseDto(Memo memo) {
+    }
+
+//    public MemoResponseDto(Memo memo) {
+//    }
 }
